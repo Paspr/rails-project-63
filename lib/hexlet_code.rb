71a999b8aec3c7735a1ef00bac7cb73546ead641
Pavel Osminin <osperevod@gmail.com>
2024-07-06 21:31:12 +0500

    feat: add form_for method

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -5,8 +5,14 @@ require_relative "hexlet_code/version"
 module HexletCode
   autoload(:Tag, "hexlet_code/tag.rb")
 
+  def self.form_for(_, url = {})
+    if url.empty?
+      "<form action=\"#\" method=\"post\"></form>"
+    else
+      "<form action=\"#{url[:url]}\" method=\"post\"></form>"
+    end
+  end
+
   class Error < StandardError; end
   # Your code goes here...
 end
-
-puts HexletCode::Tag.build("input", type: "submit", value: "Save")
