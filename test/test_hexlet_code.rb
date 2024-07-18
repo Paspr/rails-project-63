6ef38ccd92bef92d19c97bc9131bb64a0930eba6
Pavel Osminin <osperevod@gmail.com>
2024-07-18 12:36:35 +0500

    test: add new tests

--- test/test_hexlet_code.rb
+++ test/test_hexlet_code.rb
@@ -4,8 +4,10 @@ require "test_helper"
 
 class TestHexletCode < Minitest::Test
   User = Struct.new(:name, :job, keyword_init: true)
+  User2 = Struct.new(:name, :job, :gender, keyword_init: true)
   def setup
     @user = User.new name: "rob"
+    @user2 = User2.new name: "rob", job: "hexlet", gender: "m"
   end
 
   def test_that_it_has_a_version_number
@@ -19,14 +21,54 @@ class TestHexletCode < Minitest::Test
   end
 
   def test_form_user
-    expected = "<form action=\"#\" method=\"post\"></form>"
-    actual = HexletCode.form_for @user
+    expected = "<form action=\"#\" method=\"post\">\n</form>"
+    actual = HexletCode.form_for @user do |f| # rubocop:disable Lint/EmptyBlock
+    end
     assert_equal actual, expected
   end
 
   def test_form_user_with_link
-    expected = "<form action=\"/users\" method=\"post\"></form>"
-    actual = HexletCode.form_for @user, url: "/users"
+    expected = "<form action=\"/users\" method=\"post\">\n</form>"
+    actual = HexletCode.form_for @user, url: "/users" do |f| # rubocop:disable Lint/EmptyBlock
+    end
     assert_equal actual, expected
+  end
+
+  def test_default_values_case
+    expected_output_file = File.expand_path("./fixtures/default_values_case.html", __dir__)
+    expected_output = File.read(expected_output_file)
+    actual = HexletCode.form_for @user2 do |f|
+      f.input :job, as: :text
+    end
+    assert_equal actual, expected_output
+  end
+
+  def test_default_values_override_case
+    expected_output_file = File.expand_path("./fixtures/default_values_override_case.html", __dir__)
+    expected_output = File.read(expected_output_file)
+    actual = HexletCode.form_for @user2, url: "#" do |f|
+      f.input :job, as: :text, rows: 50, cols: 50
+    end
+    assert_equal actual, expected_output
+  end
+
+  def test_hash_case
+    expected_output_file = File.expand_path("./fixtures/hash_case.html", __dir__)
+    expected_output = File.read(expected_output_file)
+    actual = HexletCode.form_for @user2, url: "#" do |f|
+      f.input :name, class: "user-input"
+      f.input :job
+    end
+    assert_equal actual, expected_output
+  end
+
+  def test_base_case
+    expected_output_file = File.expand_path("./fixtures/base_case.html", __dir__)
+    expected_output = File.read(expected_output_file)
+    actual = HexletCode.form_for @user2 do |f|
+      f.input :name
+      f.input :job, as: :text
+    end
+    assert_equal actual, expected_output
   end
 end
