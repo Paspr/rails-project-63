2f7930a284707da8176e03ce55853392ab4373ce
Pavel Osminin <osperevod@gmail.com>
2024-07-18 12:37:43 +0500

    feat: rewrite form generating

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -1,16 +1,16 @@
 # frozen_string_literal: true
 
 require_relative "hexlet_code/version"
+require_relative "hexlet_code/generator"
 
 module HexletCode
   autoload(:Tag, "hexlet_code/tag.rb")
 
-  def self.form_for(_, url = {})
-    if url.empty?
-      "<form action=\"#\" method=\"post\"></form>"
-    else
-      "<form action=\"#{url[:url]}\" method=\"post\"></form>"
-    end
+  def self.form_for(user, url = {}, &block)
+    action = url.empty? ? "#" : url[:url]
+    form_builder = FormGenerator.new(user)
+    block.call(form_builder)
+    form_builder.build(action)
   end
 
   class Error < StandardError; end
