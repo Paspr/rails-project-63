2e7ac4f99892abfb9cc33a0f29451e9c0d34b352
Pavel Osminin <osperevod@gmail.com>
2024-07-18 17:16:27 +0500

    tests: add new tests

--- test/test_hexlet_code.rb
+++ test/test_hexlet_code.rb
@@ -8,6 +8,7 @@ class TestHexletCode < Minitest::Test
   def setup
     @user = User.new name: "rob"
     @user2 = User2.new name: "rob", job: "hexlet", gender: "m"
+    @user3 = User.new job: "hexlet"
   end
 
   def test_that_it_has_a_version_number
@@ -20,18 +21,20 @@ class TestHexletCode < Minitest::Test
     assert_equal actual, expected
   end
 
-  def test_form_user
-    expected = "<form action=\"#\" method=\"post\">\n</form>"
+  def test_form_user_case
+    expected_output_file = File.expand_path("./fixtures/form_user_case.html", __dir__)
+    expected_output = File.read(expected_output_file)
     actual = HexletCode.form_for @user do |f| # rubocop:disable Lint/EmptyBlock
     end
-    assert_equal actual, expected
+    assert_equal actual, expected_output
   end
 
-  def test_form_user_with_link
-    expected = "<form action=\"/users\" method=\"post\">\n</form>"
+  def test_form_user_with_link_case
+    expected_output_file = File.expand_path("./fixtures/form_user_with_link_case.html", __dir__)
+    expected_output = File.read(expected_output_file)
     actual = HexletCode.form_for @user, url: "/users" do |f| # rubocop:disable Lint/EmptyBlock
     end
-    assert_equal actual, expected
+    assert_equal actual, expected_output
   end
 
   def test_default_values_case
@@ -68,6 +71,17 @@ class TestHexletCode < Minitest::Test
     actual = HexletCode.form_for @user2 do |f|
       f.input :name
       f.input :job, as: :text
+    end
+    assert_equal actual, expected_output
+  end
+
+  def test_save_button_override_name
+    expected_output_file = File.expand_path("./fixtures/override_save_button_name_case.html", __dir__)
+    expected_output = File.read(expected_output_file)
+    actual = HexletCode.form_for @user3, url: "#" do |f|
+      f.input :name
+      f.input :job
+      f.submit "Wow"
     end
     assert_equal actual, expected_output
   end
