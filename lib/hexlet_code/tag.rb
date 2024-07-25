2cccbe644bf3e637c3442de0f15cb93e264bf43a
Pavel Osminin <osperevod@gmail.com>
2024-07-25 10:04:46 +0500

    fix: substitute if-else for one join

--- lib/hexlet_code/tag.rb
+++ lib/hexlet_code/tag.rb
@@ -4,14 +4,9 @@ module HexletCode
   module Tag
     def self.build(tag, options = {})
       attributes = options.map do |key, value|
-        "#{key}=\"#{value}\""
+        " #{key}=\"#{value}\""
       end
-      attributes.join(' ')
-      if attributes.empty?
-        "<#{tag}>"
-      else
-        "<#{tag} #{attributes.join(' ')}>"
-      end
+      "<#{tag}#{attributes.join}>"
     end
   end
 end
