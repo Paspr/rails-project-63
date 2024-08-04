6162a60781a38bdded7d4e53c2617aa6e4a6ca77
Pavel Osminin <osperevod@gmail.com>
2024-08-04 19:12:55 +0500

    feat: add inner builder

---
+++ lib/hexlet_code/builder.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: false
+
+module HexletCode
+  class Builder
+    attr_reader :form_elements
+
+    def initialize(object, url:, method:, **attributes)
+      @object = object
+      @url = url
+      @method = method
+      @attributes = attributes
+      @form_elements = []
+      @submit_text = 'Save'
+    end
+
+    def input(attribute, options = {})
+      value = @object.send(attribute)
+      type = options.fetch(:as, :input)
+      @form_elements << { type: :input, attribute:, value:, input_type: type, options: }
+    end
+
+    def submit(value = 'Save')
+      @submit_text = value
+      @form_elements << { type: :submit, value: }
+    end
+
+    def add_default_submit
+      submit(@submit_text) if @form_elements.none? { |element| element[:type] == :submit }
+    end
+  end
+end
