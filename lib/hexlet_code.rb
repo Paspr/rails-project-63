9766386b4f76557db20388000f20ff4768e97ac4
Pavel Osminin <osperevod@gmail.com>
2024-08-04 19:14:39 +0500

    feat: refactor hexlet_code to work with inner builder

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
 require_relative 'hexlet_code/version'
-# require_relative 'hexlet_code/generator'
 
 module HexletCode
   autoload(:Tag, 'hexlet_code/tag.rb')
   autoload(:Generator, 'hexlet_code/generator.rb')
+  autoload(:Builder, 'hexlet_code/builder.rb')
 
   def self.form_for(user, url: '#', method: :post, **attributes, &block)
-    form_builder = Generator.new(user, url:, method:, **attributes)
+    form_builder = Builder.new(user, url:, method:, **attributes)
     block.call(form_builder) if block_given?
-    form_builder.build
+    Generator.new(form_builder).generate
   end
 end
